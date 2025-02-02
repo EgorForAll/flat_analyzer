65e470526c444a8fd36f860661d34bdc44bf67e5
EgorForAll <eia.web.ss@gmail.com>
2025-02-02 18:45:13 +0300

    sugar valley amend

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 TELEGRAM_TOKEN = '<YOUR_TOKEN>'
 ZENIT_2 = 'https://zenit-nn.ru/flats?filter;view:list;type:flat;floor:3~15.9;rooms:3;sort:room'
-SUGAR_VALLEY = 'https://sd-nn.ru/realty?filter;sort:number;type:flat;rooms:3;floor:3.00~13.00;hide_booked:1'
+SUGAR_VALLEY = 'https://sd-nn.ru/realty?filter;sort:number;type:flat;rooms:2,3;area:61.00~99.00;floor:3.00~13.00;hide_booked:1'
 PRAGA = 'https://www.avito.ru/nizhniy_novgorod/doma_dachi_kottedzhi/taunhaus_91_m_na_uchastke_15_sot._4205255359?context=H4sIAAAAAAAA_wEfAOD_YToxOntzOjEzOiJsb2NhbFByaW9yaXR5IjtiOjA7fQseF2QfAAAA'
